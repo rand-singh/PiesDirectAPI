9808a9ce61c335dbccddbdd21388a4e1f48f7ee6
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-16 23:42:55 +0000

    Installed request package to express generated website and call the REST API

--- PiesDirectFrontEnd/routes/index.js
+++ PiesDirectFrontEnd/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const apiHelper = require('../helpers/apiHelper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  apiHelper.callApi('http://localhost:5000/api/')
+  .then(response => {
+    console.log(response);
+    res.render('index', { title: 'Pies Direct', data: response.data });
+  })
+  .catch(error => {
+    res.send(error);
+  })
+
 });
 
 module.exports = router;
